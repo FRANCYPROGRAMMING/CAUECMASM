21b3ce632f21252969fb45f6d90a85ac92ca341c
Francesca_Francyprogrammer <francescadesimone850@gmail.com>
2024-09-14 02:20:45 +0200

    Esercizio svolto

---
+++ follia.js
@@ -0,0 +1,2 @@
+import fraseJack from './frase.js';  
+fraseJack();
---
+++ frase.js
@@ -0,0 +1,5 @@
+function fraseJack() {
+    console.log("Solo lavoro e niente gioco fanno di Jack un ragazzo annoiato");
+}
+
+export default fraseJack;
